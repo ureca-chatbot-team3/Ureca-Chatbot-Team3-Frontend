31d21d75937c2b6f7119177939ef84920f8be77c
Hongjunior <seokjun09@gmail.com>
2025-06-17 19:28:57 +0900

    Feat: 헤더 구현

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,5 @@
 const MainPage = () => {
-  return <main className="bg-black text-white">테스트</main>;
+  return <main className="bg-black text-white pt-[112px] h-[2000px]">테스트</main>;
 };
 
 export default MainPage;
