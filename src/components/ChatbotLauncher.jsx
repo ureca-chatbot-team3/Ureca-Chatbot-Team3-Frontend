e1519d8ed6ba0336efa3bbd78fa614386763fd04
Youdongseok <rex010307@gmail.com>
2025-06-18 09:31:30 +0900

    feat: 챗봇 아이콘  구현

---
+++ src/components/ChatbotLauncher.jsx
@@ -0,0 +1,20 @@
+import flamingo from '../assets/chatbot.gif';
+
+export default function ChatbotLauncher({ onClick }) {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 z-50 pointer-events-none">
+      <div className="max-w-[1440px] mx-auto relative h-0">
+        <button
+          onClick={onClick}
+          className="absolute bottom-8 right-8 w-[61px] h-[61px] shadow-[0_0_4px_rgba(0,0,0,0.25)] flex items-center justify-center rounded-full pointer-events-auto"
+        >
+          <img
+            src={flamingo}
+            alt="플밍이 GIF"
+            className="w-[61px] h-[61px] object-contain pointer-events-none"
+          />
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,22 +1,27 @@
 import { Outlet } from 'react-router-dom';
+import ChatbotModal from '../components/ChatbotModal.jsx';
+import ChatbotLauncher from '../components/ChatbotLauncher';
+import { useState } from 'react';
 
 const DefaultLayout = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex flex-col h-full">
       <div className="bg-white px-[40px]">
         <div className="max-w-[1440px] mx-auto">
-          {/*  Header 컴포넌트 영역 */}
           <header>헤더</header>
         </div>
       </div>
 
       <div className="bg-gray-200 px-[40px] flex-1">
-        <div className="max-w-[1440px] mx-auto">
+        <div className="max-w-[1440px] mx-auto relative z-10">
           <Outlet />
+          <ChatbotLauncher onClick={() => setIsOpen(true)} />
+          {isOpen && <ChatbotModal onClose={() => setIsOpen(false)} />}
         </div>
       </div>
     </div>
   );
 };
-
 export default DefaultLayout;
