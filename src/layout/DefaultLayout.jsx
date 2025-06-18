44bda1a7febae9ed9e76496419c0502687a036d5
nue-os <kimsoeun0108@gmail.com>
2025-06-18 15:57:02 +0900

    Feat: 조건부 렌더링을 통한 메인페이지 레이아웃 차등 적용

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,9 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '@/components/Header';
 
 const HEADER_HEIGHT = 112;
 
 const DefaultLayout = () => {
+  const location = useLocation();
+  const isMainPage = location.pathname === '/';
+
   return (
     <div className="flex flex-col h-full">
       <div className="fixed w-full bg-white px-[40px] z-50" style={{ height: HEADER_HEIGHT }}>
@@ -12,11 +15,17 @@ const DefaultLayout = () => {
         </div>
       </div>
 
-      <div className="bg-gray-200 px-[40px] flex-1" style={{ paddingTop: HEADER_HEIGHT }}>
-        <div className="max-w-[1440px] mx-auto">
+      {isMainPage ? (
+        <div className="bg-gray-200 flex-1" style={{ paddingTop: HEADER_HEIGHT }}>
           <Outlet />
         </div>
-      </div>
+      ) : (
+        <div className="bg-gray-200 px-[40px] flex-1" style={{ paddingTop: HEADER_HEIGHT }}>
+          <div className="max-w-[1440px] mx-auto">
+            <Outlet />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
