72979e375e089bf41c928639c3747a2f8560ea56
nue-os <kimsoeun0108@gmail.com>
2025-06-19 16:23:21 +0900

    Feat: 메인 인트로 첫번째 슬라이드 구현

--- src/pages/MainPage/components/MainIntro.jsx
+++ src/pages/MainPage/components/MainIntro.jsx
@@ -1,37 +1,40 @@
 import { motion } from 'motion/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import LongArrowDownIcon from '@/assets/svg/longArrowDownIcon.svg';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import IntroSection1 from './IntroSection1';
 
 const MainIntro = () => {
   return (
-    <section className="border border-[3px] border-red-400 px-[40px] bg-linear-[to_bottom,var(--color-pink-300),var(--color-white)] h-[calc(100vh-var(--header-height))]">
-      <div
-        className={`border border-[3px] content-max-width mx-auto flex flex-col items-center h-full`}
-      >
+    <section className="px-[40px] bg-linear-[to_bottom,var(--color-pink-300),var(--color-white)] h-[calc(100vh-var(--header-height))]">
+      <div className="content-max-width mx-auto flex flex-col items-center h-full gap-3 relative">
         <Swiper
           spaceBetween={30}
           centeredSlides={true}
           autoplay={{
             delay: 4000,
-            disableOnInteraction: false,
+            disableOnInteraction: true,
           }}
           pagination={{
             clickable: true,
           }}
           navigation={true}
           modules={[Autoplay, Pagination, Navigation]}
-          className="w-full h-full"
+          className="w-full h-full mb-[100px]"
         >
-          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>
+            <IntroSection1 />
+          </SwiperSlide>
           <SwiperSlide>Slide 2</SwiperSlide>
           <SwiperSlide>Slide 3</SwiperSlide>
         </Swiper>
         {/* 임시 화살표 */}
         <motion.div
-          className="text-4xl font-bold h-[100px] flex items-end"
+          className="absolute bottom-0"
+          style={{ aspectRatio: '16/102' }}
           animate={{ y: [0, -10, 0] }} // 위로 갔다가 다시 제자리
           transition={{
             duration: 1.5,
@@ -40,7 +43,7 @@ const MainIntro = () => {
             ease: 'easeInOut',
           }}
         >
-          ⬇️
+          <img src={LongArrowDownIcon} alt="아래로" />
         </motion.div>
       </div>
     </section>
