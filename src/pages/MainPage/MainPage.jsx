bd4cb0385156652c447595bd830d9ed462568a98
nue-os <kimsoeun0108@gmail.com>
2025-06-19 13:27:40 +0900

    Feat: 메인페이지 첫번째 섹션과 두번째 섹션 사이 자동 스크롤 구현

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,14 +1,22 @@
+import { useRef } from 'react';
 import BackgroundWrapper from './components/BackgroundWrapper';
 import FirstSection from './components/FirstSection';
 import SecondSection from './components/SecondSection';
 import ThirdSection from './components/ThirdSection';
+import { useAutoScrollToSection } from '@/hooks/useAutoScroll';
 
 const MainPage = () => {
+  const secondRef = useRef(null);
+
+  useAutoScrollToSection(secondRef);
+
   return (
     <main>
       <FirstSection />
       <BackgroundWrapper>
-        <SecondSection />
+        <div ref={secondRef}>
+          <SecondSection />
+        </div>
         <ThirdSection />
       </BackgroundWrapper>
     </main>
