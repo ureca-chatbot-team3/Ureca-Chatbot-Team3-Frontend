2275fc2c0ca8ef103081c0d6f7b5e147db178422
nue-os <kimsoeun0108@gmail.com>
2025-06-20 15:42:41 +0900

    Style: 진단 안내 섹션 투명도 애니메이션 적용

--- src/pages/MainPage/components/DiagnosisIntro.jsx
+++ src/pages/MainPage/components/DiagnosisIntro.jsx
@@ -20,13 +20,17 @@ const SecondSection = () => {
 
   return (
     <section ref={ref} className="content-max-width mx-auto h-[calc(100vh-var(--header-height))]">
-      <div className="flex flex-col gap-md justify-center items-center h-full" style={{ opacity }}>
+      <motion.div
+        className="flex flex-col gap-md justify-center items-center h-full"
+        style={{ opacity }}
+      >
         <h2 className="heading-1 font-500">맞춤 요금제 진단</h2>
-        <p className="heading-3 font-300">간단한 질문을 통해, 딱 맞는 요금제를 추천해 드려요</p>
-        <div className="p-md max-w-[802px] bg-white flex flex-col w-full justify-center items-center gap-md rounded-[20px] shadow-[0px_0px_10px_0px_rgba(0,0,12,0.08)]">
-          <div className="">
-            <img src={DiagnosisImage} alt="맞춤 요금제 진단 안내" />
-          </div>
+        <p className="heading-3 font-300 mb-sm">
+          간단한 질문을 통해, 딱 맞는 요금제를 추천해 드려요
+        </p>
+        <div className="p-lg max-w-[802px] bg-white flex flex-col w-full justify-center items-center gap-md rounded-[20px] shadow-[0px_0px_10px_0px_rgba(0,0,12,0.08)]">
+          <img src={DiagnosisImage} alt="맞춤 요금제 진단 안내" />
+
           <div className="flex justify-between max-w-[410px] w-full">
             <div className="flex gap-2 items-center">
               <img src={QuestionICon} alt="질문 수" />
@@ -44,7 +48,7 @@ const SecondSection = () => {
             진단 시작하기
           </button>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
