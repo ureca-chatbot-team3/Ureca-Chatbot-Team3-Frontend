9469aecf410d09db8550ecf5f5d9e87a3a0019e9
nue-os <kimsoeun0108@gmail.com>
2025-06-24 17:38:23 +0900

    Stlye: 반응형 스타일 수정

--- src/pages/MainPage/components/ChatbotIntro.jsx
+++ src/pages/MainPage/components/ChatbotIntro.jsx
@@ -4,19 +4,19 @@ import PhonePlanQuestionListImage from '@/assets/svg/PhonePlanQuestionList.svg';
 
 const ThirdSection = () => {
   return (
-    <section className="content-max-width mx-auto h-[calc(100vh-var(--header-height))]">
+    <section className="content-max-width mx-auto md:h-[calc(100vh-var(--header-height))]">
       <motion.div
-        className="flex flex-col gap-2xl justify-center items-center h-full"
+        className="py-2xl md:py-xl flex flex-col gap-md md:gap-2xl items-center h-full"
         initial={{ y: 50, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ type: 'spring', duration: 3 }}
         viewport={{ once: false, amount: 0.3 }}
       >
-        <h2 className="heading-1 font-500">
+        <h2 className="m-heading-2 font-700 md:heading-1 md:font-500">
           <span className="text-pink-700">AI 챗봇</span>을 사용해보세요
         </h2>
 
-        <div className="max-w-[1080px] w-full flex justify-around items-center gap-md">
+        <div className="md:max-w-[1080px] w-full flex flex-col md:flex-row justify-around items-center gap-md">
           <ChatCard
             title="요금제에 대한 궁금한 내용 질문"
             image={PhonePlanQuestionImage}
@@ -38,19 +38,22 @@ const ThirdSection = () => {
 
 const ChatCard = ({ title, image, alt, description, delay = 0 }) => (
   <motion.div
-    className="flex flex-col items-center bg-white gap-sm rounded-[20px] shadow-[0px_0px_10px_0px_rgba(0,0,12,0.08)] px-md py-xl max-w-[400px] w-full max-h-[508px] h-full"
+    className="flex flex-col items-center md:bg-white gap-sm rounded-[20px] md:shadow-[0px_0px_10px_0px_rgba(0,0,12,0.08)] px-md md:py-xl md:max-w-[400px] w-full md:max-h-[508px] h-full"
     initial={{ y: 50, opacity: 0 }}
     whileInView={{ y: 0, opacity: 1 }}
     transition={{ type: 'spring', duration: 3, delay }}
     viewport={{ once: false, amount: 0.3 }}
   >
-    <h3 className="heading-3 font-500">{title}</h3>
-    <div className="gap-sm flex flex-col flex-1 justify-around">
-      <div className="max-h-[240px] h-full flex justify-center items-center">
+    <h3 className="hidden md:heading-3 font-500">{title}</h3>
+    <div className="gap-sm flex flex-col flex-1 justify-around items-center">
+      <div className="max-w-[180px] md:max-w-[240px] h-full flex justify-center items-center">
         <img src={image} alt={alt} className="object-contain h-full w-full" />
       </div>
-      <p className="body-medium font-400 text-center whitespace-pre-line">{description}</p>
+      <p className="hidden md:block body-medium font-400 text-center whitespace-pre-line">
+        {description}
+      </p>
     </div>
+    <h3 className="md:hidden m-body-large font-700">{title}</h3>
   </motion.div>
 );
 
