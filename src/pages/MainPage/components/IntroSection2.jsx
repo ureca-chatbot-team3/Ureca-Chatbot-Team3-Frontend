f9171332ff372b12efb13252677611c9d76871e1
nue-os <kimsoeun0108@gmail.com>
2025-06-24 16:37:59 +0900

    Style: 메인 페이지 첫 번째 섹션 반응형 구현

--- src/pages/MainPage/components/IntroSection2.jsx
+++ src/pages/MainPage/components/IntroSection2.jsx
@@ -9,17 +9,19 @@ export const IntroSection2 = ({ isActive }) => {
   const heights = [`calc(${baseHeight} - 40px)`, `calc(${baseHeight} - 20px)`, baseHeight];
 
   return (
-    <section className="flex flex-col w-full h-full px-xl max-w-[1114px] gap-4">
-      <h1 className="heading-1 font-500 px-4">
+    <section className="flex flex-col w-full h-full md:px-xl max-w-[1114px] gap-4">
+      <h1 className="text-center md:text-left m-heading-3 font-700 md:heading-1 md:font-500 md:px-4">
         전화 기다릴 필요 없이 <br />
-        <span className="text-pink-700">지금 바로 AI 챗봇에게 요금제를 물어보세요!</span>
+        <span className="text-pink-700">
+          <span className="hidden md:block">지금 바로</span> AI 챗봇에게 요금제를 물어보세요!
+        </span>
       </h1>
 
-      <div className="flex items-end">
+      <div className="flex items-end justify-center">
         {[ChatExample1Image, ChatExample2Image, ChatExample3Image].map((src, idx) => (
           <motion.div
             key={idx}
-            className="flex"
+            className={`flex ${idx === 0 ? 'block' : 'hidden md:flex'}`}
             style={{ maxHeight: heights[idx] }}
             initial={{ opacity: 0, x: -50 }}
             animate={
@@ -36,10 +38,34 @@ export const IntroSection2 = ({ isActive }) => {
                   }
             }
           >
-            <img src={src} alt={`요금제 예시${idx + 1}`} className="max-h-full object-contain" />
+            <img
+              src={src}
+              alt={`요금제 예시${idx + 1}`}
+              className="max-h-[160px] md:max-h-full md:object-contain"
+            />
           </motion.div>
         ))}
       </div>
+
+      <motion.div
+        className="m-body-medium font-500 text-center md:hidden"
+        initial={{ opacity: 0, x: -50 }}
+        animate={
+          isActive
+            ? {
+                opacity: 1,
+                x: 0,
+                transition: { duration: 0.6, delay: 0.4, ease: 'easeOut' },
+              }
+            : {
+                opacity: 0,
+                x: -50,
+                transition: { delay: 0, ease: 'easeOut' },
+              }
+        }
+      >
+        아이콘을 눌러 AI 챗봇과 상담을 시작하세요!
+      </motion.div>
     </section>
   );
 };
