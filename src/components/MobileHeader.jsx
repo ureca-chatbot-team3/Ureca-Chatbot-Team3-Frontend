a056c01acebcd226169d27b53da285d7d8d760b5
yshls <shluxnsal01@gmail.com>
2025-06-25 16:44:44 +0900

    Feat: header 검색 아이콘 클릭시 PlanListPage 검색 input 포커스 이동 기능 추가

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -21,6 +21,11 @@ const MobileHeader = () => {
     }
   };
 
+  const handleGoToSearch = () => {
+    navigate('/plans?focusSearch=true');
+    setMenuOpen(false);
+  };
+
   return (
     <header className="bg-white border-b border-gray-300 fixed top-0 left-0 w-full z-50">
       {/* 상단바 */}
@@ -31,7 +36,7 @@ const MobileHeader = () => {
         </Link>
 
         {/* 오른쪽 아이콘 */}
-        <div className="flex items-center gap-4">
+        <div onClick={handleGoToSearch} className="flex items-center gap-4">
           <button className="p-0 m-0 bg-transparent border-none">
             <img src={searchIcon} alt="검색" className="w-5 h-5" />
           </button>
